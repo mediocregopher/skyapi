462246ca201f109994dc2c2a39b323aebda7f6f2
Brian Picciano <mediocregopher@gmail.com>
2015-05-19 17:15:03 -0400

    first iteration of skyapi

---
+++ README.md
@@ -0,0 +1,47 @@
+# skyapi
+
+A simple websocket API which sits in front of SkyDNS/etcd and manages active
+processes for services.
+
+Processes make a long-held websocket connection to skyapi with information about
+the service they're providing for, their host/port, and optionally weight or
+priority (See the [SkyDNS docs](https://github.com/skynetservices/skydns) for
+more on weight/priority). When the connection is closed the entry for that
+process will be removed from its service.
+
+## DNS Entries
+
+SkyDNS always has a root domain it serves, and all of its entries must be under
+this domain. The root domain is configurable, but skyapi must also know about
+it. For the rest of the examples we're going to say the root domain is
+`turtles.com`.
+
+skyapi adds service entries to the `services` sub-domain. For example, if a
+process creates a connection saying it provides the `foo` service then it will
+be added to the pool under `foo.services.turtle.com`.
+
+## Build
+
+    go get github.com/mediocregopher/skyapi
+    cd $GOPATH/src/github.com/mediocregopher/skyapi
+    go build
+
+## Usage
+
+skyapi listens by default on port 8053. It exposes a single websocket endpoint,
+`/provide`. This endpoint accepts the following GET parameters:
+
+* **service** (Required) - The name of the service being provided
+* **host** (Required) - The host/ip the process can be reached on
+* **ip** (Required) - The port the process can be reached on
+* **priority** - The priority the process should be given in its pool. Default 1.
+* **weight** - The weight the process should be given in its pool. Default 100.
+
+For example: `GET /provide?service=foo&host=127.0.0.1&port=9999&priority=1`
+
+Once the websocket is created a corresponding DNS entry will be created.
+
+The client *should* periodically send websocket pings to the server, and the
+server will also send pings periodically. If the server's pings to the client
+fail it will immediately remove the client's entry from the service pool it was
+providing for.
---
+++ main.go
@@ -0,0 +1,261 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/coreos/go-etcd/etcd"
+	"github.com/gorilla/websocket"
+	"github.com/mediocregopher/lever"
+)
+
+var (
+	listenAddr string
+	etcdAPIs   []string
+	dnsRoot    string
+	timeout    time.Duration
+	etcdClient *etcd.Client
+)
+
+func main() {
+	l := lever.New("skyapi", nil)
+	l.Add(lever.Param{
+		Name:        "--listen-addr",
+		Description: "Address to listen for websocket connections on",
+		Default:     ":8053",
+	})
+	l.Add(lever.Param{
+		Name:         "--etcd-api",
+		Description:  "scheme://address an etcd node in the cluster can be found on. Can be specified multiple times",
+		DefaultMulti: []string{"http://127.0.0.1:4001"},
+	})
+	l.Add(lever.Param{
+		Name:        "--dns-root",
+		Description: "Root domain that SkyDNS is serving on",
+		Default:     "skydns.local.",
+	})
+	l.Add(lever.Param{
+		Name:        "--timeout",
+		Description: "The TTL for entries in SkyDNS, in seconds. The server will ping at half this value, the client should also",
+		Default:     "30",
+	})
+	l.Parse()
+
+	listenAddr, _ = l.ParamStr("--listen-addr")
+	etcdAPIs, _ = l.ParamStrs("--etcd-api")
+	dnsRoot, _ = l.ParamStr("--dns-root")
+	dnsRoot = dnsRootToPath(dnsRoot)
+	timeoutSecs, _ := l.ParamInt("--timeout")
+	timeout = time.Duration(timeoutSecs) * time.Second
+
+	etcdClient = etcd.NewClient(etcdAPIs)
+
+	http.Handle("/provide", http.HandlerFunc(handler))
+	log.Printf("listening on %s", listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
+
+func dnsRootToPath(root string) string {
+	parts := strings.Split(root, ".")
+	partsR := append(make([]string, 0, len(parts)+2), "/skydns")
+	for i := len(parts) - 1; i >= 0; i-- {
+		partsR = append(partsR, parts[i])
+	}
+	partsR = append(partsR, "services")
+	return path.Join(partsR...)
+}
+
+func randID() string {
+	b := make([]byte, 10)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(b)
+}
+
+var upgrader = websocket.Upgrader{
+	// Buffer sizes are 0 because nothing should ever be read or written
+	ReadBufferSize:  0,
+	WriteBufferSize: 0,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type connData struct {
+	id, service, host      string
+	port, priority, weight int
+}
+
+func (cd connData) sprintf(s string, args ...interface{}) string {
+	realArgs := make([]interface{}, 0, len(args)+4)
+	realArgs = append(realArgs, cd.id, cd.service, cd.host, cd.port)
+	realArgs = append(realArgs, args...)
+	return fmt.Sprintf("[%s - %s] %s:%d - "+s, realArgs...)
+}
+
+func errorMessagef(conn *websocket.Conn, s string, args ...interface{}) {
+	msg := fmt.Sprintf("ERROR: "+s, args...)
+	conn.WriteMessage(websocket.TextMessage, []byte(msg))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("error upgrading: %s", err)
+		return
+	}
+	closeCh := make(chan struct{})
+	go readDiscard(conn, closeCh)
+	defer conn.Close()
+
+	cd, err := parseConnData(r)
+	if err != nil {
+		log.Printf("parseConnData: %s", err)
+		errorMessagef(conn, "parseConnData: %s", err)
+		return
+	}
+
+	defer log.Print(cd.sprintf("closed"))
+	log.Print(cd.sprintf("connected"))
+
+	tick := time.Tick(timeout / 2)
+	if !doTick(conn, cd) {
+		return
+	}
+	for {
+		select {
+		case <-tick:
+			if !doTick(conn, cd) {
+				return
+			}
+		case <-closeCh:
+			return
+		}
+	}
+}
+
+func doTick(conn *websocket.Conn, cd connData) bool {
+	deadline := time.Now().Add(timeout / 2)
+	err := conn.WriteControl(websocket.PingMessage, nil, deadline)
+	if err != nil {
+		log.Print(cd.sprintf("timedout"))
+		return false
+	}
+
+	if err = etcdStore(cd); err != nil {
+		log.Print(cd.sprintf("storing etcd data: %s", err))
+		errorMessagef(conn, "storing etcd data: %s", err)
+		return false
+	}
+	return true
+}
+
+func readDiscard(conn *websocket.Conn, closeCh chan struct{}) {
+	for {
+		if _, _, err := conn.NextReader(); err != nil {
+			close(closeCh)
+			return
+		}
+	}
+}
+
+func parseConnData(r *http.Request) (connData, error) {
+	service := r.FormValue("service")
+	host := r.FormValue("host")
+	portStr := r.FormValue("port")
+	if service == "" || host == "" || portStr == "" {
+		err := fmt.Errorf("service, host, and port are all required parameters")
+		return connData{}, err
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return connData{}, err
+	}
+
+	var priority int
+	priorityStr, weightStr := r.FormValue("priority"), r.FormValue("weight")
+	if priorityStr == "" {
+		priority = 1
+	} else {
+		if priority, err = strconv.Atoi(priorityStr); err != nil {
+			return connData{}, err
+		}
+	}
+
+	var weight int
+	if weightStr == "" {
+		weight = 100
+	} else {
+		if weight, err = strconv.Atoi(weightStr); err != nil {
+			return connData{}, err
+		}
+	}
+
+	return connData{
+		id:       randID(),
+		service:  service,
+		host:     host,
+		port:     port,
+		priority: priority,
+		weight:   weight,
+	}, nil
+}
+
+// Creates the given dir (and all of its parent directories if they don't
+// already exist). Will not return an error if the given directory already
+// exists
+func MkDirP(ec *etcd.Client, dir string) error {
+	parts := make([]string, 0, 4)
+	for {
+		parts = append(parts, dir)
+		dir = path.Dir(dir)
+		if dir == "/" {
+			break
+		}
+	}
+
+	for i := range parts {
+		ai := len(parts) - i - 1
+		_, err := ec.CreateDir(parts[ai], 0)
+		if err != nil && err.(*etcd.EtcdError).ErrorCode != 105 {
+			return err
+		}
+	}
+	return nil
+}
+
+func etcdStore(cd connData) error {
+	dir := path.Join(dnsRoot, cd.service)
+	if err := MkDirP(etcdClient, dir); err != nil {
+		return err
+	}
+
+	file := path.Join(dir, cd.id)
+	j, err := json.Marshal(map[string]interface{}{
+		"host":     cd.host,
+		"port":     cd.port,
+		"priority": cd.priority,
+		"weight":   cd.weight,
+	})
+	if err != nil {
+		return err
+	}
+
+	_, err = etcdClient.Set(file, string(j), uint64(timeout.Seconds()))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
